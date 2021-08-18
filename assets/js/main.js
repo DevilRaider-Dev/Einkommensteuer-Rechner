18c2663bcae24a99478ac21044f7704f3fb7cfec
DevilRaider-Dev <deviltwix3@gmail.com>
2021-08-18 15:35:40 +0200

    finalize

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,8 @@
-/*
-In diesem etwas umfangreicheren Projekt schreiben wir eine Einkommensteuer-Berechnungs-App.
-Wir werden die Daten von https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2021 verwenden.
-
-Erst definieren wir die “zu versteuerndes Einkommen” (zvE) Formel.
-
-Das Design ist frei!
-
-Nach der Berechnung des zVE gibt es 5 Fälle die eintreten können (abhängig vom Jahreseinkommen). Erstelle eine WebSite, mit der wir die Einkommensteuer berechnen können. Nutze dazu die Links als Referenz.
-Wir sollten eine Möglichkeit haben, das Jahr zu wählen, das wir berechnen wollen.
-
-
-2021: https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2021
-2020: https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2020
-2019: https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2019
-*/
 
 let obj = { input: 0, year: 0, married: false, est: 0, res: 0 };
 let calcObj = { yA: 0, estA: 0, estB: 0, estC: 0, }
 
 function switchMarried() {
-
     if (readInput().married) {
         document.getElementById("incomeYear2Container").style.display = "flex";
     } else {
@@ -39,8 +22,8 @@ function readInput() {
     return obj;
 }
 
-function numberWithDots(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+function numberWithDots(string) {
+    return string.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
 function writeOutput() {
@@ -121,6 +104,7 @@ function calc() {
             }
             break;
     }
+    
     obj.est = res;
     obj.res = obj.input - res;
 }
