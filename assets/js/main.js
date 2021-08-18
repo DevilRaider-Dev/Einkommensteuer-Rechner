2c69e0fdb86ffdc5ef42a9f12880a6721a563cf7
DevilRaider-Dev <deviltwix3@gmail.com>
2021-08-18 13:37:02 +0200

    bu push 2021

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,17 +15,64 @@ Wir sollten eine Möglichkeit haben, das Jahr zu wählen, das wir berechnen woll
 2019: https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2019
 */
 
-function readInput(){
-    let input = document.getElementById("incomeYear").value;
-    let year = document.getElementById("taxYear").value;
-    let married = false;
-    if(document.getElementById("marriedYes").checked){
+let obj = { input: 0, year: 0, married: false, split: 0, est: 0, res: 0 };
 
+function switchMarried() {
+
+    if (readInput().married) {
+        document.getElementById("incomeYear2Container").style.display = "flex";
+    } else {
+        document.getElementById("incomeYear2Container").style.display = "none";
     }
-    console.log(input + " " + year)
 }
 
-function calcTax(){
-    document.getElementsByClassName("resultContainer")[0].style.display = "block"
-    readInput();
+function readInput() {
+
+    let input = Number(document.getElementById("incomeYear").value);
+    let year = document.getElementById("taxYear").value;
+    let married = false;
+
+    if (document.getElementById("marriedYes").checked) {
+        married = true;
+    }
+
+    obj.input = input;
+    obj.year = year
+    obj.married = married;
+
+    return obj;
+}
+
+function writeOutput() {
+    document.getElementById("bruttoRes").innerHTML = obj.input.toFixed(2) + " €";
+    document.getElementById("estRes").innerHTML = obj.est.toFixed(2) + " €";
+    document.getElementById("nettoRes").innerHTML = obj.res.toFixed(2) + " €";
+}
+
+function calcSplitting() {
+
+    if (obj.married) {
+        res = (obj.input + Number(document.getElementById("incomeYear2").value)) / 2;
+    } else {
+        res = obj.input;
+    }
+
+    return res;
+}
+
+function calcTax() {
+
+    obj = readInput();
+    obj.split = calcSplitting();
+
+    if (obj.input < 9745) {
+        obj.res = obj.input;
+        writeOutput();
+    } else if(o9744) {
+
+    }
+
+    console.log(obj);
+
+    document.getElementsByClassName("resultContainer")[0].style.display = "block";
 }
